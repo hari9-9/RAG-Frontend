337e811999fddf28eb13de4227bdd5ceaaf31710
Hari <reachk.hariharan@gmail.com>
2025-02-28 00:20:03 +0000

    removed liniting errors

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import PdfReactPdf from './components/pdfView'
-import PageLayout from './components/pageLayout'
 
 import PdfViewerPage from './components/PdfViewerPage'
 
